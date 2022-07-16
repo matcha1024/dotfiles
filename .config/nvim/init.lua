315fa78beb308aef0fbea9c2b901bb0e2233ee31
matcha1024 <matcha@matcha1024.net>
2022-07-16 22:55:17 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,7 +5,7 @@ require("keymap")
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.tabstop = 4
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
 vim.cmd("colorscheme tokyonight")
 vim.notify = require("notify")
